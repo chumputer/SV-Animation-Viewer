f7bf5740ca55f051ae6cd727691611b31c080ff0
chumputer <chumcoder@protonmail.com>
2022-01-22 02:54:16 -0800

    Init commit

---
+++ precache-manifest.36b0b7d2e3b2f89274afb5a82bb75d56.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c188c1c757d9f3733e238648287dd7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2cd25921167b15e7dbb1",
+    "url": "./static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ced0e9735587ecb71db9",
+    "url": "./static/js/2.661a3389.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.661a3389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2271765d6cf8a0a732fe",
+    "url": "./static/js/3.be4f24b8.chunk.js"
+  },
+  {
+    "revision": "2018fdf15d0379ed37a5325858bf6030",
+    "url": "./static/js/3.be4f24b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e679818ef0445fa5af93",
+    "url": "./static/js/4.0cae8f29.chunk.js"
+  },
+  {
+    "revision": "2cd25921167b15e7dbb1",
+    "url": "./static/js/main.22c26a45.chunk.js"
+  },
+  {
+    "revision": "4a33f1b063370d6c03a8",
+    "url": "./static/js/runtime-main.d45a06fb.js"
+  }
+]);
